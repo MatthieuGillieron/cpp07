8b970e6ffc04aadee4e8c2aceeb1760635349b53
MatthieuGillieron <gillieron.matthieu@ik.me>
2026-02-24 10:27:32 +0100

    add: function for compare min/max value + test

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,7 +1,36 @@
 
-
+#include "whatever.hpp"
 
 int main()
 {
 	
+	int a = 6;
+	int b = 1;
+	int c = 6;
+
+
+	std::cout << std::endl; 
+	std::cout << "max >: ";
+	std::cout << max<int>(a, b) << std::endl;
+	
+
+	std::cout << std::endl;
+	std::cout << "min > : ";
+	std::cout << min<int>(a, b) << std::endl;
+
+
+	std::cout << std::endl;
+	std::cout << "max equal: ";
+	std::cout << max<int>(a, c) << std::endl;
+	std::cout << "adresse c: " << &c << std::endl;
+	std::cout << "adresse retour : " << &(min(a, c)) << std::endl;
+
+
+	std::cout << std::endl;
+	std::cout << "min equal: ";
+	std::cout << min<int>(a, c) << std::endl;
+
+	std::cout << "adresse c: " << &c << std::endl;
+	std::cout << "adresse retour : " << &(min(a, c)) << std::endl;
+
 }
---
+++ ex00/whatever.hpp
@@ -0,0 +1,28 @@
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <string>
+#include <iostream>
+
+template<typename T>
+const T& max(const T& a, const T& b)
+{
+	if (a <= b)
+		return b;
+	else
+		return a;
+}
+
+template<typename T>
+const T& min(const T& a, const T& b)
+{
+	if (a < b)
+		return a;
+	else
+		return b;
+}
+
+
+#endif
+  
