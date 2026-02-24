42079c15d80eb0b72d0db68b9c1e91e48273f0b2
MatthieuGillieron <gillieron.matthieu@ik.me>
2026-02-24 11:29:10 +0100

    add: better log

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,42 +1,71 @@
-
 #include "whatever.hpp"
 
 int main()
 {
-	
 	int a = 6;
 	int b = 1;
 	int c = 6;
 
-	std::cout << "=== TEST MIN/MAX ===" << std::endl;
+	std::string d = "chaine 1";
+	std::string e = "chaine 2";
+	std::string f = "chaine 1";
 
-	std::cout << "a = 6,  b = 1 | max: ";
-	std::cout << max<int>(a, b) << std::endl;
+
+	std::cout << "=== TEST MIN/MAX INT ===" << std::endl;
+
+	std::cout << "a = " << a << ",  b = " << b << " | max: ";
+	std::cout << ::max(a, b) << std::endl;
 	
-	
-	std::cout << "a = 6,  b = 1 | min: ";
-	std::cout << min<int>(a, b) << std::endl;
-	
-	
-	std::cout << std::endl;
-	std::cout << "a = 6,  c = 6 | max equal: ";
-	std::cout << max<int>(a, c) << std::endl;
-	std::cout << "adresse c: " << &c << std::endl;
-	std::cout << "adresse retour : " << &(min(a, c)) << std::endl;
-	
+	std::cout << "a = " << a << ",  b = " << b << " | min: ";
+	std::cout << ::min(a, b) << std::endl;
 	
 	std::cout << std::endl;
-	std::cout << "a = 6,  c = 6 | min equal: ";
-	std::cout << min<int>(a, c) << std::endl;
-	
+	std::cout << "a = " << a << ",  c = " << c << " | max equal: ";
+	std::cout << ::max(a, c) << std::endl;
 	std::cout << "adresse c: " << &c << std::endl;
-	std::cout << "adresse retour : " << &(min(a, c)) << std::endl;
+	std::cout << "adresse retour : " << &(::max(a, c)) << std::endl;
+	
+	std::cout << std::endl;
+	std::cout << "a = " << a << ",  c = " << c << " | min equal: ";
+	std::cout << ::min(a, c) << std::endl;
+	std::cout << "adresse c: " << &c << std::endl;
+	std::cout << "adresse retour : " << &(::min(a, c)) << std::endl;
 	std::cout << std::endl; 
+
+
+	std::cout << "=== TEST MIN/MAX STRING ===" << std::endl;
+
+	std::cout << "d = " << d << ",  e = " << e << " | max: ";
+	std::cout << ::max(d, e) << std::endl;
+	
+	std::cout << "d = " << d << ",  e = " << e << " | min: ";
+	std::cout << ::min(d, e) << std::endl;
+	
+	std::cout << std::endl;
+
+	std::cout << "d = " << d << ",  f = " << f << " | max equal: ";
+	std::cout << ::max(d, f) << std::endl;
+	std::cout << "adresse f: " << &f << std::endl;
+	std::cout << "adresse retour : " << &(::max(d, f)) << std::endl;
+	
+	std::cout << std::endl;
+
+	std::cout << "d = " << d << ",  f = " << f << " | min equal: ";
+	std::cout << ::min(d, f) << std::endl;
+	std::cout << "adresse f: " << &f << std::endl;
+	std::cout << "adresse retour : " << &(::min(d, f)) << std::endl;
+	std::cout << std::endl; 
+
 
 	std::cout << "=== TEST SWAP ===" << std::endl;
 
-	std::cout << "value a: " << a << " value b: " << b << std::endl;
-	swap(a, b);
-	std::cout << "swap a -> b: " << a << " swap b -> a: " << b << std::endl;
+	std::cout << "value a: " << a << " |  value b: " << b << std::endl;
+	::swap(a, b);
+	std::cout << "swap a -> b | a = " << a << " swap b -> a | b = " << b << std::endl;
 
+	std::cout << std::endl;
+
+	std::cout << "value d : " << d << " |  value e : " << e << std::endl;
+	::swap(d, e);
+	std::cout << "swap d -> e | d = " << d << " swap e -> d | e = " << e << std::endl;
 }
