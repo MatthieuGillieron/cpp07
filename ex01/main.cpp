64a73583fa58dc2f63afd0a277410b2778564a89
MatthieuGillieron <gillieron.matthieu@ik.me>
2026-02-25 20:21:50 +0100

    add: better log for eval

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,17 +2,24 @@
 #include "iter.hpp"
 
 template <typename T>
+void print(T const  &val)
+{
+	std::cout << val << " ";
+}
+
+
+template <typename T>
 void x2(T &val)
 {
 	val *= 2;
 } 
 
-
 template <typename T>
-void print(T &val)
+void toUpper(T &val)
 {
-	std::cout << val << " ";
+	val -= 32;
 }
+
 
 int main()
 {
@@ -28,9 +35,19 @@ int main()
 	iter(tab1, 3, print<int>);
 
 	std::cout << std::endl;
+	std::cout << std::endl;
 
 	std::cout << "=== TEST CHAR ===" << std::endl;
 	char tab2[] = {'a', 'b', 'c'};
 	std::cout << "Beginning value: ";
 	iter(tab2, 3, print<char>);
+
+	std::cout << std::endl;
+
+	std::cout << "to upper: ";
+	iter(tab2, 3, toUpper<char>);
+	iter(tab2, 3, print<char>);
+
+	std::cout << std::endl;
+
 }
